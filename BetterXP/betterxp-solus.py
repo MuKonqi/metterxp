897e177c4a61561e29ee8a99594c8046a0d8ad2f
Muhammed Abdurrahman <82509753+MuKonqi@users.noreply.github.com>
2021-12-07 20:16:30 +0300

    Hatalar giderildi.
    "BetterXP'ı Yeniden Başlat" özelliğindeki hatalar giderildi ve iyileştirildi.

--- BetterXP/betterxp-solus.py
+++ BetterXP/betterxp-solus.py
@@ -26,15 +26,7 @@ def programkapat():
     print("\nBetterXP kapatılıyor...")
     exit()
 def bxpreopen():
-    if os.path.isfile("/etc/debian-version"):
-        print("\nBetterXP yeniden başlatılıyor...")
-        pencere.destroy()
-        os.system("python3 /usr/local/bin/BetterXP/betterxp-debian.py")
-    elif os.path.isfile("/etc/fedora-release"):
-        print("\nBetterXP yeniden başlatılıyor...")
-        pencere.destroy()
-        os.system("python3 /usr/local/bin/BetterXP/betterxp-fedora.py")
-    elif os.path.isfile("/etc/solus-release"):
+    if os.path.isfile("/etc/solus-release"):
         print("\nBetterXP yeniden başlatılıyor...")
         pencere.destroy()
         os.system("python3 /usr/local/bin/BetterXP/betterxp-solus.py")
@@ -699,7 +691,7 @@ def snapkur():
         messagebox.showerror("Hata","Bazı hata(lar) oluştu!\n'OK' tuşuna basınca tekrar denemeniz için program kapatılacak.")
         programkapat()   
 def info():
-    yazi2.config(text="BetterXP, son kullanıcı için tasarlanmış, özgür bir yazılım ürünüdür.\nBetterXP, GNU General Public License, Version 3.0 (GPL 3) altında lisanslanmıştır.\nYapımcı: MuKonqi (Muhammed Abdurrahman)\nTemel: Terminalden kurtulun 2.0 (Kararlı Sürüm)\nSürüm: 2.0.3 (Kararlı Sürüm)")
+    yazi2.config(text="BetterXP, son kullanıcı için tasarlanmış, özgür bir yazılım ürünüdür.\nBetterXP, GNU General Public License, Version 3.0 (GPL 3) altında lisanslanmıştır.\nYapımcı: MuKonqi (Muhammed Abdurrahman)\nTemel: Terminalden kurtulun 2.0 (Kararlı Sürüm)\nSürüm: 2.0.3-2 (Kararlı Sürüm)")
     islemsecimbuton1.config(text="İletişim",command=contact)
     islemsecimbuton2.config(text="Yenilikler",command=yenilikler)
     islemsecimbuton3.config(text="Lisans",command=lisans)
@@ -1104,7 +1096,7 @@ else:
     pencere=Tk()
     pencere.title("BetterXP'a Hoş Geldiniz")
     pencere.config(background="#000000")
-    pencere.resizable(0, 0) 
+    pencere.resizable(0, 0)     
 
     b_metin1=Label(pencere, background="#000000", foreground="#FFFFFF", font="arial 3")
 
