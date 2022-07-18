d901bb4b9884a575238b3f74038d61ad53162c2a
Muhammed Abdurrahman <82509753+MuKonqi@users.noreply.github.com>
2022-07-18 13:47:50 +0300

    Update yukselt.py

--- modules/yukselt.py
+++ modules/yukselt.py
@@ -5,19 +5,16 @@
 # This file part of MetterXP.
 
 import os
-import sys
 
 debian="/etc/debian_version"
 fedora="/etc/fedora-release"
 solus="/etc/solus-release"
-args=sys.argv[1:]
 
 if not os.path.isdir("/usr/local/bin/metterxp/apiutaller"):
     os.system("cd /usr/local/bin/metterxp/ ; mkdir apiutaller")
 os.system("cd /usr/bin/ ;  rm metterxp")
-os.system("cd /usr/share/polkit-1/actions ;  rm io.github.mukonqi.metterxp.policy")
 os.system("cd /usr/share/applications ;  rm metterxp.desktop")
-os.system("cd /usr/local/bin/metterxp/ ;  rm -rf modules ; rm -rf unsupported.app ; rm icon.png ;  rm LICENSE.txt")
+os.system("cd /usr/local/bin/metterxp/ ;  rm -rf modules ; rm -rf unsupported.app ; rm icon.png ; rm LICENSE")
 os.system(" chmod +x /usr/local/bin/metterxp/metterxp/app/metterxp.py ; mv /usr/local/bin/metterxp/metterxp/app/metterxp.py /usr/bin/metterxp")
 os.system(" chmod +x /usr/local/bin/metterxp/metterxp/apiutaller.py ; mv /usr/local/bin/metterxp/metterxp/apiutaller.py /usr/local/bin/metterxp/apiutaller")
 os.system(" chmod +x /usr/local/bin/metterxp/metterxp/app/io.github.mukonqi.metterxp.policy ; mv /usr/local/bin/metterxp/metterxp/app/io.github.mukonqi.metterxp.policy /usr/share/polkit-1/actions/")
@@ -25,5 +22,4 @@ os.system(" chmod +x /usr/local/bin/metterxp/metterxp/app/metterxp.desktop ; mv 
 os.system(" chmod +x /usr/local/bin/metterxp/metterxp/app/unsupported.app/* ;  mv /usr/local/bin/metterxp/metterxp/app/unsupported.app /usr/local/bin/metterxp/")
 os.system(" chmod +x /usr/local/bin/metterxp/metterxp/app/modules/* ; mkdir /usr/local/bin/metterxp/modules ; mv /usr/local/bin/metterxp/metterxp/app/modules/* /usr/local/bin/metterxp/modules ;  mv /usr/local/bin/metterxp/metterxp/app/icon.png /usr/local/bin/metterxp/ ;  mv /usr/local/bin/metterxp/metterxp/app/LICENSE.txt /usr/local/bin/metterxp/")
 os.system("cd /usr/local/bin/metterxp/ ;  rm -rf metterxp")
-os.system("cd /usr/local/bin/metterxp/modules/ ; rm yukselt.py")
 exit()
