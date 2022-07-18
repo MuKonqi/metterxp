466b39b5fecf568af17e2b3790ac46a6a9bfca5f
Muhammed Abdurrahman <82509753+MuKonqi@users.noreply.github.com>
2022-07-18 13:08:16 +0300

    Update yukselt.py

--- modules/yukselt.py
+++ modules/yukselt.py
@@ -12,15 +12,14 @@ fedora="/etc/fedora-release"
 solus="/etc/solus-release"
 args=sys.argv[1:]
 
-if "--yukseltiyukselt" in args:
-    if os.path.isfile(debian):
-        os.system(" apt install git -y")
-    elif os.path.isfile(fedora):
-        os.system(" dnf install git -y")
-    elif os.path.isfile(solus):
-        os.system(" eopkg install git -y")    
-    os.system("cd /usr/local/bin/metterxp ;  git clone https://github.com/MuKonqi/metterxp ; cd metterxp ;  chmod +x *")
-    os.system("cd /usr/local/bin/metterxp/modules ; rm yukselt.py")
-    os.system(" cp /usr/local/bin/metterxp/metterxp/modules/mxp_update.py /usr/local/bin/metterxp/modules/")
-    os.system(" python3 /usr/local/bin/metterxp/modules/mxp_update.py")
-    exit()
+if os.path.isfile(debian):
+    os.system(" apt install git -y")
+elif os.path.isfile(fedora):
+    os.system(" dnf install git -y")
+elif os.path.isfile(solus):
+    os.system(" eopkg install git -y")    
+os.system("cd /usr/local/bin/metterxp ;  git clone https://github.com/MuKonqi/metterxp ; cd metterxp ;  chmod +x *")
+os.system("cd /usr/local/bin/metterxp/modules ; rm yukselt.py")
+os.system(" cp /usr/local/bin/metterxp/metterxp/modules/mxp_update.py /usr/local/bin/metterxp/modules/")
+os.system(" python3 /usr/local/bin/metterxp/modules/mxp_update.py")
+exit()
